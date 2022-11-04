642d3181c8fd158c98631b5a4fd5d9a9552e9e34
baziz_meriem <mrmbaziz@gmail.com>
2022-11-04 19:57:57 +0100

    hero section

--- website/src/App.js
+++ website/src/App.js
@@ -1,4 +1,5 @@
-import Navbar from "./components/Navbar";
+
+import Hero from "./components/Hero";
 import {BrowserRouter as Router} from 'react-router-dom';
 import { GlobalStyle } from "./components/GlobalStyles";
 
@@ -6,7 +7,7 @@ function App() {
   return (
     <Router>
       <GlobalStyle/>
-      <Navbar/>
+      <Hero/>
     </Router>
      
     
