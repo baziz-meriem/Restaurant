7ced28036aa9bb8c5cae9a20d63a0e5d3bf219bb
baziz_meriem <mrmbaziz@gmail.com>
2022-11-19 23:41:07 +0100

    navbar

--- website/src/components/Navbar/Navbar.jsx
+++ website/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React , { useState } from 'react';
 
 import { FaPizzaSlice} from 'react-icons/fa';
 import { MdOutlineRestaurantMenu } from 'react-icons/md';
 import './Navbar.css';
 import LOGO from '../../images/gericht.png';
 
-const Navbar = ()=>(
-    <nav className='app_navbar'>
+const Navbar = ()=>{
+    const [toggleMenu,setToggleMenu] = useState(false);
+    return (
+        <nav className='app_navbar'>
         <div className="app__navbar-logo">
             <img src={LOGO} alt="logo"/>
         </div>
@@ -20,20 +22,26 @@ const Navbar = ()=>(
         <div className="app__navbar-login">
             <a href="#login" className="p__opensans" >Log In/Register</a>
         </div>
-        <div className="app_navbar-smallscreen">
-            <FaPizzaSlice clor="#fff fontSize={27}" className="overlay__close" onClick={ ()=>{} } />
-            <div className="app__navbar-smalscreen_overlay flex__center slide-bottom">
-            <ul className="app_navbar-smallscreen-links" >
-                <li className="p__opensans"><a href="#" >Home</a></li>
-                <li className="p__opensans"><a href="#" >About</a></li>
-                <li className="p__opensans"><a href="#" >Menu</a></li>
-                <li className="p__opensans"><a href="#" >Contact</a></li>
-                <li className="p__opensans"><a href="#" >Contact</a></li>
-            </ul>
+        <div className="app__navbar-smallscreen">
+            <FaPizzaSlice color="#fff" fontSize={27}  className="overlay__open" onClick={ ()=> setToggleMenu(true) } />
+
+            {toggleMenu && (
+            <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
+                <MdOutlineRestaurantMenu color="#fff" fontSize={27} className="overlay__close" onClick={()=> { setToggleMenu(false)}}/>
+                <ul className="app__navbar-smallscreen_overlay app_navbar-smallscreen-links" >
+                    <li className="p__opensans"><a href="#" >Home</a></li>
+                    <li className="p__opensans"><a href="#" >About</a></li>
+                    <li className="p__opensans"><a href="#" >Menu</a></li>
+                    <li className="p__opensans"><a href="#" >Desert</a></li>
+                    <li className="p__opensans"><a href="#" >Contacts</a></li>
+                </ul>
             </div>
-            
+            )}
         </div>
        
     </nav>
-);
+
+    )
+    
+};
 export default Navbar;
